da5b5f7fbb3a4fd2e65a54ee3579fc93453276a2
christianraymond <ombenisn.raymond@gmail.com>
2017-09-30 16:28:01 +0200

    Refused to reload font because it violates the following Content Security Policy directive: default-src self. Note that font-src was not explicitly set, so default-src is used as a fallback.

--- public/shoeApi.js
+++ public/shoeApi.js
@@ -1,15 +1,38 @@
 $(function() {
-  var url = "http://localhost:3000/shoes"
+  var url = "http://localhost:3000/shoes";
   var shoesTable = document.getElementById("shoesTable").innerHTML;
   var compiledTable = Handlebars.compile(shoesTable);
   var showTable = document.getElementById("showTable");
 
-  $.ajax({
-    type: "get",
-    url: url,
+  //Add new brands to the database using POST.
+
+  $("#addBtn".onclick, function(){
+    var stock = {
+      brand: $brand.val(),
+      color: $color.val(),
+      size: $size.val(),
+      instock: $in_stock.val()
+    }
+    $.ajax({
+      type: "POST",
+      data: stock,
+      dataType: "aplication/json",
+      url: url,
       success: function(data){
+        console.log("New shoes has been successfully added!");
+      },
+      error: function(error){
+        alert("I getting an error message!")
+      }
+    })
+  });
+  //Get all brand from the database.
+  $.ajax({
+    type: "GET",
+    url: url,
+    success: function(data) {
       showTable.innerHTML = compiledTable({
-       Shoes: data
+        Shoes: data
       })
     },
     error: function(error) {
